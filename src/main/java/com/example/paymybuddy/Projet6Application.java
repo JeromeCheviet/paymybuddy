b6d006f4824e0389adf47c1705004dbdc85e5749
Jerome Cheviet <admin@gharan.ovh>
2022-04-24 19:27:37 +0200

    Init spring project.

---
+++ src/main/java/com/example/paymybuddy/Projet6Application.java
@@ -0,0 +1,13 @@
+package com.example.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projet6Application.class, args);
+    }
+
+}
